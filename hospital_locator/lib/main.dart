e85aca771c9adc92c72921736291c1807bcc643f
Emmanuel <Emmanuel@DESKTOP-T83DPON>
2021-10-31 02:59:22 -0700

    project setup complete

--- hospital_locator/lib/main.dart
+++ hospital_locator/lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(HospitalLocator());
 }
 
-class HospitalLocator extends StatelessWidget {
+class HospitalLocator extends StatefulWidget {
+  @override
+  _HospitalLocatorState createState() => _HospitalLocatorState();
+}
+
+class _HospitalLocatorState extends State<HospitalLocator> {
+  CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   final String _title = "Hospital locator";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +25,9 @@ class HospitalLocator extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        body: GoogleMap(initialCameraPosition: _cameraPosition),
+      ),
     );
   }
 }
